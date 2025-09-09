484a8c322fa66d7a02680884a5a814c90af1c96c
Sofia de Alcantara <sofiaalcantaras@outlook.com>
2025-09-09 20:01:20 -0300

    add simple form

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export interface UserInput {
+    streamingHours: number;
+    emailsSent: number;
+    socialMediaUsage: number;
+    cloudStorage: number;
+}
+
+export interface CarbonFootprint {
+    totalFootprint: number;
+    averageFootprint: number;
+}
